e095d07c2beb9780c85cef86cbcbc9e18f8f6b2e
Randall Sutton <rsutton@devclock.com>
2021-10-30 13:30:07 -0600

    added ability to send ether

--- src/web/index.js
+++ src/web/index.js
@@ -3,7 +3,9 @@ var app = new Vue({
     data: {
         activated: false,
         message: "Welcome to VideoBox!",
-        walletAvailable: false
+        toAddress: "",
+        walletAvailable: false,
+        walletConnected: false
     },
     mounted() {
         this.activate();
@@ -12,15 +14,65 @@ var app = new Vue({
         activate() {
             this.activated = true;
 
+            // Make sure a wallet is available.
             if(ethereum !== typeof(undefined))
             {
-                this.walletAvailable = true;
-            }
+                console.log("ethereum found");
 
-            console.log("activated");
+                this.walletAvailable = true;
+
+                // Show the connect button if the wallet isn't connected.
+                if(ethereum.isConnected() == false)
+                {
+                    console.log("ethereum not connected");
+                }
+                else
+                {
+                    console.log("ethereum connected");
+
+                    this.walletConnected = true;
+
+                    this.setupWeb3();
+                }
+            }
+            else 
+            {
+                console.log("ethereum not found");
+            }
+        },
+        connectWallet() {
+            console.log("connecting to wallet");
+
+            ethereum.request({ method: 'eth_requestAccounts' });
+
+            this.setupWeb3();
+
+            console.log("connected to wallet");
+        },
+        sendToAddress() {
+            console.log(`sending to ${this.toAddress}`);
+
+            let transaction = {
+                from: ethereum.selectedAddress,
+                to: this.toAddress,
+                value: web3.utils.numberToHex(web3.utils.toWei("0.01"))
+            };
+
+            ethereum.request({ method: 'eth_sendTransaction', params: [transaction]});
+        },
+        setupWeb3() {
+            console.log("web3 setup");
+
+            web3 = new Web3(ethereum);
         }
     },
     computed: {
+        connectWalletVisible() {
+            return this.walletAvailable === false;
+        },
+        sendVisible() {
+            return this.walletConnected === true;
+        },
         walletStatusMessage() {
             let ret = "Available";
 
